cf955966ae0d5c4131c272562f69dda467ec39e6
Susana <susanahernandezey@gmail.com>
2019-11-14 18:35:41 +0100

    Roman Numeral: convert 9 to it\s equivalent to roman number IX

--- roman-numerals/src/roman-numerals.js
+++ roman-numerals/src/roman-numerals.js
@@ -2,6 +2,8 @@ export const numberToRoman = (number) => {
   let output = ''
   if(number === 4) {
     output = 'IV'
+  } else if (number === 9) {
+    output = 'IX'
   } else if( number >= 5 ) {
     output += 'V'
 
--- roman-numerals/test/roman-numerals.test.js
+++ roman-numerals/test/roman-numerals.test.js
@@ -8,7 +8,9 @@ describe('Roman Numerals should', () => {
     [4, 'IV'],
     [5, 'V'],
     [6, 'VI'],
-    [7, 'VII']
+    [7, 'VII'],
+    [8, 'VIII'],
+    [9, 'IX']
   ].forEach(([number, roman]) => {
     it(`convert ${number} to it\s equivalent to roman number ${roman}`, () => {
       expect(numberToRoman(number)).toBe(roman)
