78522e06ef0d469adf6f2940bff2f280f64fc244
Susana <susanahernandezey@gmail.com>
2019-11-14 16:01:53 +0100

    Return false if year is divisible by 100 but not 400

--- leap-years/test/leap-years.test.js
+++ leap-years/test/leap-years.test.js
@@ -5,6 +5,12 @@ describe('Leap years should', () => {
     it(`return true if the year (${year}) is divisible by 400`, () => {
       expect(isLeapYear(year)).toBe(true)
     })
-  })
+  });
+
+  [1700, 1800, 1900, 2100].forEach((year) => {
+    it('return false if the year is divisible by 100 but not by 400', () => {
+      expect(isLeapYear(year)).toBe(false)
+    })
+  });
   
 })
