c05ed5d284e9a60d052357c2080f472e9092f230
Susana <susanahernandezey@gmail.com>
2019-11-17 15:13:25 +0100

    Roman Numeral: back to init

--- roman-numerals/src/roman-numerals.js
+++ roman-numerals/src/roman-numerals.js
@@ -1,27 +1,9 @@
-export const numberToRoman = (number) => {
+export function toRoman(number) {
   let output = ''
-
-  if(number === 4) {
-    return 'IV'
-  } 
-
-  if (number === 9) {
-    return 'IX'
-  }
-
-  if( number >= 10 ) {
-    output += 'X'
-    number = number - 10
-  }
-
-  if( number >= 5 ) {
-    output += 'V'
-    number = number - 5
-  }
 
   for (let i = 0; i < number; i++) {
     output += 'I'
   }
 
   return output
-} 
+}
--- roman-numerals/test/roman-numerals.test.js
+++ roman-numerals/test/roman-numerals.test.js
@@ -1,22 +1,13 @@
-import { numberToRoman } from '@/roman-numerals'
+import { toRoman } from '@/roman-numerals'
 
 describe('Roman Numerals should', () => {
   [
     [1, 'I'],
     [2, 'II'],
     [3, 'III'],
-    [4, 'IV'],
-    [5, 'V'],
-    [6, 'VI'],
-    [7, 'VII'],
-    [8, 'VIII'],
-    [9, 'IX'],
-    [10, 'X'],
-    [11, 'XI'],
-    [14, 'XIV']
-  ].forEach(([number, roman]) => {
-    it(`convert ${number} to it\s equivalent to roman number ${roman}`, () => {
-      expect(numberToRoman(number)).toBe(roman)
+  ]  .forEach(([number, romanNumber]) => {
+    it(`converts ${number} to it's roman representation ${romanNumber}`, () => {
+      expect(toRoman(number)).toBe(romanNumber)
     })
-  })
+  }) 
 })
