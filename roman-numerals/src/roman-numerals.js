bb40923444f3b9b68b82e3f228ca941595b377f3
Susana <susanahernandezey@gmail.com>
2019-11-17 15:16:46 +0100

    Convert numbers between 5 and 8

--- roman-numerals/src/roman-numerals.js
+++ roman-numerals/src/roman-numerals.js
@@ -1,6 +1,15 @@
 export function toRoman(number) {
   let output = ''
 
+  if(number === 4) {
+    return 'IV'
+  }
+
+  if(number >= 5) {
+    output += 'V'
+    number -= 5
+  }
+
   for (let i = 0; i < number; i++) {
     output += 'I'
   }
--- roman-numerals/test/roman-numerals.test.js
+++ roman-numerals/test/roman-numerals.test.js
@@ -5,6 +5,10 @@ describe('Roman Numerals should', () => {
     [1, 'I'],
     [2, 'II'],
     [3, 'III'],
+    [4, 'IV'],
+    [5, 'V'],
+    [6, 'VI'],
+    [8, 'VIII'],
   ]  .forEach(([number, romanNumber]) => {
     it(`converts ${number} to it's roman representation ${romanNumber}`, () => {
       expect(toRoman(number)).toBe(romanNumber)
