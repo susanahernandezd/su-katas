1bde7adcb4acbdab450b2c3d09c29bb0d6a46549
Susana <susanahernandezey@gmail.com>
2019-11-17 15:26:34 +0100

    Create a pattern passing all to while loop

--- roman-numerals/src/roman-numerals.js
+++ roman-numerals/src/roman-numerals.js
@@ -1,28 +1,29 @@
 export function toRoman(number) {
   let output = ''
 
-  if(number >= 10) {
+  while(number >= 10) {
     output += 'X'
     number -= 10
   }
 
-  if(number === 9) {
+  while(number === 9) {
     output += 'IX'
     number -= 9
   }
 
-  if(number >= 5) {
+  while(number >= 5) {
     output += 'V'
     number -= 5
   }
 
-  if(number >= 4) {
+  while(number >= 4) {
     output += 'IV'
     number -= 4
   }
 
-  for (let i = 0; i < number; i++) {
+  while(number > 0) {
     output += 'I'
+    number -= 1
   }
 
   return output
