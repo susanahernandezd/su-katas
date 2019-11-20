92e2323fe07e4a9ca3dd48e1d2ecbdd85f390ae5
Susana <susanahernandezey@gmail.com>
2019-11-20 18:27:48 +0100

    Do the sum of all numbers in the string

--- string-calculator/src/string-calculator.js
+++ string-calculator/src/string-calculator.js
@@ -1,6 +1,12 @@
-export function stringCalculator (string) {
-  if (string === '4') {
-    return 4
+export function stringCalculator (stringNumbers) {
+  if (!stringNumbers) {
+    return 0
   }
-  return 0
+
+  const sum = stringNumbers.split(',').reduce((acc, string) => {
+    acc = acc + parseInt(string)
+    return acc
+  }, 0)
+
+  return sum
 }
--- string-calculator/test/string-calculator.test.js
+++ string-calculator/test/string-calculator.test.js
@@ -3,9 +3,15 @@ import { stringCalculator } from '@/string-calculator'
 describe('String Calculator should', () => {
   it(`sum 0 when it doesn\t receive anything`, () => {
     expect(stringCalculator("")).toBe(0)
-  })
+  });
 
-  it('sum the string number', () => {
-    expect(stringCalculator("4")).toBe(4)
+  [
+    ['4', 4],
+    ['1,2', 3],
+    ['1,2,3,4,5,6,7,8,9', 45]
+  ].forEach(([string, resultSum]) => {
+    it(`sum the string numbers (${string}) returning ${resultSum}`, () => {
+      expect(stringCalculator(string)).toBe(resultSum)
+    })
   })
 })
