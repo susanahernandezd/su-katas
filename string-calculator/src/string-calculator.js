cc61406aa1f54dfbfb35398c761dfc07bff01ab4
Susana <susanahernandezey@gmail.com>
2019-11-30 10:25:14 +0100

    use the "domain" concepts present in the requirements

--- string-calculator/src/string-calculator.js
+++ string-calculator/src/string-calculator.js
@@ -6,18 +6,16 @@ const Regex = {
   BETWEEN_BRAQUETS: /\[(.*?)\]/g
 }
 
-//Try to use the "domain" concepts present in the requirements
-// `string` is not a concept is the type of the argument
-export function stringCalculator (string) {
+export function stringCalculator (numbers) {
   // Rethink the concept of "options"
   // and try to find another way to do the `getCustomOptions`
   let options = {
     separators: SEPARATORS,
-    stringNumbers: string
+    numbers
   }
 
-  if (hasCustomSeparator(string)) {
-    options = getCustomOptions(string)
+  if (hasCustomSeparator(numbers)) {
+    options = getOptions(numbers)
   }
 
   const negatives = negativeNumbers(options)
@@ -29,8 +27,8 @@ export function stringCalculator (string) {
 
 }
 
-function sum ({ stringNumbers, separators }) {
-  return stringNumbers.split(new RegExp(separators)).reduce((acc, string) => {
+function sum ({ numbers, separators }) {
+  return numbers.split(new RegExp(separators)).reduce((acc, string) => {
     const number = parseInt(string)
     acc = acc + (number < MAX_NUM ? number : 0)
     return acc
@@ -41,18 +39,18 @@ function hasCustomSeparator (string) {
   return string.startsWith('//')
 }
 
-function getCustomOptions (string) {
+function getOptions (string) {
   const separatorString = string.split(Regex.SEPARATOR_STRING).filter((values) => values)
   const multipleSeparator = separatorString[0].match(Regex.BETWEEN_BRAQUETS)
 
   return {
     separators: multipleSeparator ? multipleSeparator.join('|') : separatorString[0],
-    stringNumbers: separatorString[1]
+    numbers: separatorString[1]
   }
 }
 
-function negativeNumbers ({ stringNumbers, separators }) {
-  return stringNumbers.split(new RegExp(separators)).reduce((acc, number) => {
+function negativeNumbers ({ numbers, separators }) {
+  return numbers.split(new RegExp(separators)).reduce((acc, number) => {
     if (number < 0) {
       acc += ` ${number}`
     }
